555339babede48ce0f5a2227661dcd58500d94d4
Atikabdessamad <Atikabdessamad24@gmail.com>
2026-04-22 12:54:01 +0200

    Ajout interface utilisateur

---
+++ script.js
@@ -0,0 +1,5 @@
+ function changer() {
+      document.getElementById("title").textContent = "Bravo 😄";
+      document.getElementById("text").textContent = "Tu viens de cliquer sur le bouton !";
+      document.getElementById("card").classList.toggle("active");
+    }
